cf80a0ac54712d0afe12e585ca94e217757ec4f2
Matt Wood <matt9663@gmail.com>
2019-09-25 12:15:54 -0400

    pushing to GH for submission

---
+++ src/composition/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import List from './List.js';
+
+function App(props) {
+  console.log(props);
+  return (
+    <main className="App">
+      <header className="App-header">
+        <h1>
+          Trelloyes!
+        </h1>
+      </header>
+      <div className="App-list">
+        {props.store.lists.map((list) =>
+          <List 
+            key={list.id} 
+            header={list.header}
+            cards={list.cardIds.map(id => props.store.allCards[id])}         
+          />)        
+        }
+      </div>   
+    </main>
+  );
+}
+
+export default App;
